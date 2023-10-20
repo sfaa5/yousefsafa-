4489de2aecf07d5a60e73a030a9377a26ef284db
sfaa5 <113928633+sfaa5@users.noreply.github.com>
2023-10-20 07:02:05 -0700

    Add files via upload

--- testt.c
+++ testt.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct login                           // before the first use of `l`.
 {
@@ -8,97 +9,24 @@ struct login                           // before the first use of `l`.
     char lname[30];
     char username[30];
     char password[20];
+   
 };
 
-void login (void);
-void registration (void);
+
 
 int main (void)
 {
-    int option;
+      struct login l; 
+        bool f;
+       int active;
 
-    printf("Press '1' to Register\nPress '2' to Login\n\n");
-    scanf("%d",&option);
+           printf("\nEnter the Activation");
 
-    getchar();           // catching newline.
+          scanf("%d",&active);
 
-    if(option == 1)
-        {
-            system("CLS");
-            registration();
-        }
+        f = active ;
 
-    else if(option == 2)
-        {
-            system("CLS");
-            login();
-        }
-}
-
-void login (void)
-{
-    char username[30],password[20];
-    FILE *log;
-
-    log = fopen("login.txt","r");
-    if (log == NULL)
-    {
-        fputs("Error at opening File!", stderr);
-        exit(1);
-    }
-
-    struct login l;
-
-    printf("\nPlease Enter your login credentials below\n\n");
-    printf("Username:  ");
-    fgets(username, 30, stdin);
-    printf("\nPassword: ");
-    printf("\n");
-    fgets(password, 20, stdin);
-
-    while(fread(&l,sizeof(l),1,log))
-        {
-        if(strcmp(username,l.username)==0 && strcmp(password,l.password)==0)
-
-            {   
-                printf("\nSuccessful Login\n");
-            }
-        else 
-            {
-                printf("\nSuccessful Login\n");
-            }
-        }
-
-    fclose(log);
-
-    return;
-}
-
-
-
-
-void registration(void)
-{
-    char firstname[15];
-    FILE *log;
-
-    log=fopen("login.txt","w");
-    if (log == NULL)
-    {
-        fputs("Error at opening File!", stderr);
-        exit(1);
-    }
-
-
-    struct login l;
-
-    printf("\nWelcome to your online course provider. We need to enter some details for registration.\n\n");
-    printf("\nEnter First Name:\n");
-    scanf("%c",l.fname);
-    printf("\nEnter Surname:\n");
-    scanf("%s",l.lname);
-
-    printf("Thank you.\nNow please choose a username and password as credentials for system login.\nEnsure the username is no more than 30 characters long.\nEnsure your password is at least 8 characters long and contains lowercase, uppercase, numerical and special character values.\n"); 
+   
 
     printf("\nEnter Username:\n");
     scanf("%s",l.username);
@@ -106,13 +34,46 @@ void registration(void)
     scanf("%s",l.password);
 
 
-    fwrite(&l,sizeof(l),1,log);
-    fclose(log);
+ 
 
-    printf("\nConfirming details...\n...\nWelcome, %s!\n\n",firstname);
-    printf("\nRegistration Successful!\n");
-    printf("Press any key to continue...");
         getchar();
-    system("CLS");
-    login();
+
+        if(f){
+
+         char usernamee[30],passwordd[20];
+   
+
+  
+
+    
+
+    printf("\nPlease Enter your login credentials below\n\n");
+    printf("Username:  ");
+    
+    scanf("%s",usernamee);
+
+    /*fgets(username, 30, stdin);*/
+    printf("\nPassword: ");
+    printf("\n");
+   /* fgets(password, 20, stdin);*/
+    scanf("%s",passwordd);
+    
+       int r1 = strcmp(usernamee,l.username);
+       int r2 = strcmp(passwordd,l.password);
+       
+       
+     
+
+        if(r1&&r2)
+        {
+             printf("\nno Login\n"); 
+        }else{
+           printf("\nSuccessful Login\n"); 
+        }
+       
+        
+return 0 ;
+ 
 }
+}
+
